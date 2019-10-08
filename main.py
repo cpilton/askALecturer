df8db451c17dde6e5bff3e4b8281d944f3fcbfd8
Callum Pilton <callum.pilton@ibm.com>
2019-10-08 17:36:40 +0100

    Initial Commit
    Added basic homepage, html, font, and css files.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/callum")
-def salvador():
-    return "Hello, Callum"
 
 if __name__ == "__main__":
     app.run(debug=True)
