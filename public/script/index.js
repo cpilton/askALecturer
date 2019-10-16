eebd248e5bcc4c7d7744a9749481f49662ec3a31
Callum Pilton <callum.pilton@ibm.com>
2019-10-16 17:04:49 +0100

    NLU Response Shown
    Reponse from NLU Emotion module is shown

--- public/script/index.js
+++ public/script/index.js
@@ -1,14 +1,47 @@
+var typingTimer;
+var doneTypingInterval = 1000;
+
 $(document).ready(function () {
     $('#question').focus(function () {
         $('#ask-container').css('height', '200px')
     });
 });
 
-function submitQuestion() {
-    const question = $('#question').val();
-    analyzeText(question)
+function questionInput() {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(callNlu, doneTypingInterval);
 }
 
+//Call the Natural Language Understanding Ajax Function
+function callNlu() {
+    const question = $('#question').val();
+    if (question.length > 10) {
+        analyzeText(question);
+    }
+}
+
+function nluCallback(nluResponse) {
+    console.log(nluResponse);
+    const emotion = nluResponse.results.result.emotion.document.emotion;
+    console.log(emotion);
+    var highestValue = getHighestValue(emotion);
+    $('#emotion').css('background-image',' url("/img/emotion/'+highestValue[0]+'.svg")');
+}
+
+function getHighestValue(obj) {
+    var item, max = 0;
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (obj[key] > max) {
+                max = obj[key];
+                item = key;
+            }
+        }
+    }
+    return [item,max];
+}
+
+//Requests a response from the Watson Natural language API based on text given
 function analyzeText(text) {
     $.ajax({
         method: "POST",
@@ -18,6 +51,7 @@ function analyzeText(text) {
             text: text
         })
     }).done(function (data) {
-        console.log(data);
+        nluCallback(data);
+        return data;
     });
 }
