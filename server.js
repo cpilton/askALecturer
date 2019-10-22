2210a71a90ae3c3292c6c733de874e56542b6319
Callum Pilton <callum.pilton@ibm.com>
2019-10-22 22:50:40 +0100

    Added Cron and ability to Post
    - Added cron task to count users online
    - Added ability to post questions

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
+// Initialise the express module
 const express = require('express');
 const app = express();
+
+// Initialise the http module
 const server = require('http').Server(app);
+
+// Initialise the socket.io module
 const io = require('socket.io')(server);
+
+// Initialise the express-session module
 const session = require('express-session');
+
+// Initialise the memorystore module
 const MemoryStore = require('memorystore')(session);
+
+// Initialise the body-parser module
+const bodyParser = require('body-parser');
+
+// Initialise the ibm-watson natural-language-understanding module
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+
+// Initialise the ibm-watson auth module
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+// Initialise the google-cloud firestore module
+const Firestore = require('@google-cloud/firestore');
+
+// Start the app on port 3000 in development mode, or use an environment specified port in production mode
 app.set('port', process.env.PORT || 3000);
 server.listen(app.get('port'));
-const bodyParser = require('body-parser');
-const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
-const Firestore = require('@google-cloud/firestore');
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client('112598048766-mm93uk5ukmjdcvae6dnit6qfq1qc4gj6.apps.googleusercontent.com');
 
-//Set up express
+// Tell express where static files are kept
 app.use(express.static(__dirname + '/public'));
 
-//Set up a sticky session to ensure real-time messages work over multiple instances
+// Set up a sticky session to ensure real-time messages work over multiple instances
 app.use(bodyParser.json());
 app.use(
   session({
@@ -34,15 +51,25 @@ app.use(
   })
 );
 
-// load  local authentication configuration and service credentials
+// Load local authentication configuration and service credentials for IBM
 let localAuth;
 try {
     localAuth = require('./auth-local.json');
-  console.log("App started");
-  console.log("Loaded local Authentication");
-} catch (e) {}
+  console.log("Loaded IBM Authentication");
+} catch (e) {
+    console.log("Failed to open IBM Local");
+}
 
-//Connect to the Natural Language API
+// Load local authentication configuration and service credentials for GAE
+let gaeAuth;
+try {
+    gaeAuth = require('./gae-local.json');
+    console.log("Loaded GAE Authentication");
+} catch (e) {
+    console.log("Failed to open GAE Local");
+}
+
+// Connect to the Natural Language API
 const nlu = new NaturalLanguageUnderstandingV1({
   version: '2019-07-12',
   authenticator: new IamAuthenticator({
@@ -51,7 +78,7 @@ const nlu = new NaturalLanguageUnderstandingV1({
   url: localAuth.services.nlu.url,
 });
 
-//connect to the Firestore API
+// Connect to the Firestore API
 const db = new Firestore({
     projectId: 'ask-a-lecturer',
     keyFilename: './gae-local.json',
@@ -64,24 +91,32 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/tasks/countUsers', function (req, res) {
+    res.json({users: users});
+});
+
 //User Functions
 app.post("/user/verify", function (request, response) {
-    var token = request.body;
-
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: '112598048766-mm93uk5ukmjdcvae6dnit6qfq1qc4gj6.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
-            // Or, if multiple clients access the backend:
-            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-        });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
-        // If request specified a G Suite domain:
-        //const domain = payload['hd'];
-    }
-    verify().catch(console.error);
+    var token = request.body.token;
+    console.log(token)
+    client.verifyIdToken({
+        idToken: token,
+        audience: gaeAuth.client_id // If you have multiple [CLIENT_ID_1, CLIENT_ID_2, ...]
+    }, (err, login) => {
+        console.log(err);
+        console.log(login);
+        response.send(login);
+    });
 });
+
+async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: gaeAuth.client_id  // Specify the CLIENT_ID of the app that accesses the backend
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+}
 
 
 //DB Functions
@@ -93,6 +128,7 @@ app.post("/db/addQuestion", function (request, response) {
     let docRef = db.collection('questions').doc();
     docRef.set(data);
 
+    io.emit('newQuestion', data);
     response.sendStatus(200);
 });
 
@@ -113,11 +149,14 @@ app.get("/db/getQuestions", function (request, response) {
 });
 
 // Connection function
+var users = 0;
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  users++;
+    io.emit('userCount', users);
   // Each socket also fires a special disconnect event
   socket.on('disconnect', function () {
-    console.log('a user disconnected');
+    users--;
+      io.emit('userCount', users);
   });
 });
 
